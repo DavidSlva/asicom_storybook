3a359768925552e99d9d71f2a4ad6e3041f9ac1c
DavidSlva <davidsilvaurrutia@gmail.com>
2024-01-25 03:47:15 -0300

    default initialization

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/react";
+import "../src/styles/colors.css";
+import "../src/styles/generics.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
